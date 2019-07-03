1aa8efebdccf0a5678f93fdffc564f0bb6e16215
BrittleFoot <i_o@rtural.ru>
2019-07-03 17:06:02 +0500

    + extraction zone

--- Source/FPS/FPSExtractionZone.h
+++ Source/FPS/FPSExtractionZone.h
@@ -6,21 +6,35 @@
 #include "GameFramework/Actor.h"
 #include "FPSExtractionZone.generated.h"
 
+
+class UBoxComponent;
+class UDecalComponent;
+
+
 UCLASS()
-class FPS_API AFPSExtractionZone : public AActor
-{
-	GENERATED_BODY()
-	
-public:	
-	// Sets default values for this actor's properties
-	AFPSExtractionZone();
+class FPS_API AFPSExtractionZone : public AActor {
+    GENERATED_BODY()
+
+public:
+    // Sets default values for this actor's properties
+    AFPSExtractionZone();
 
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+    UPROPERTY(VisibleAnywhere, Category = "Components")
+    UBoxComponent* OverlapComponent;
+
+    UPROPERTY(VisibleAnywhere, Category = "Components")
+    UDecalComponent* DecalComponent;
+
+    UFUNCTION()
+    void HandleOverlap(
+            UPrimitiveComponent *OverlappedComp,
+            AActor *OtherActor,
+            UPrimitiveComponent *OtherComp,
+            int32 OtherBodyIndex,
+            bool bFromSweep,
+            const FHitResult &SweepResult
+    );
 
 };
